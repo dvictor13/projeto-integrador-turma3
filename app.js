3f3b5d2611c35fc8fa301239bb1d94587cc3af5b
Julio Mattos <juliocastellobranco@gmail.com>
2022-07-05 20:00:24 -0300

    not found page redirection

--- app.js
+++ app.js
@@ -22,6 +22,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.use((req,res)=>{
+  res.status(404).render('not-found')
+})
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
