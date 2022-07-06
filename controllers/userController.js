7165dd2070f481878612960d228a48d17e76b12f
Julio Mattos <juliocastellobranco@gmail.com>
2022-07-06 00:41:31 -0300

    depiulação

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const listaUsuarios =[
-    {nome:'Fulano de Tal',plano:"BASIC",mes:{barba:3,cabelo:2,depilacao:1},total:{cabelos:22,barbas:32,depilacao:5},barbearia:"BARBEARIA BAIRRO BONFIM"}
+    {nome:'Fulano de Tal',plano:"BASIC",mes:{barba:3,cabelo:2,depilação:1},total:{cabelos:22,barbas:32,depilacao:5},barbearia:"BARBEARIA BAIRRO BONFIM"}
 ]
 const listaPlanos = [
     {nome:"BASIC",preco:"R$30,00",barbas:1,cabelos:1,img:"images/barbeiros/barbeiro_detalhe_barba_tesoura.jpg",vantagens:["Barbas mais robustas","Ajustes mensais"],economia:20},
