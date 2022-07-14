8700f49b655fe45817962e5e5a727a2de681afd5
Julio Mattos <juliocastellobranco@gmail.com>
2022-07-13 23:25:29 -0300

    turn user database

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,8 @@
-const fs = require('fs')
+const fs = require('fs');
 
-const listaUsuarios =[
-    {nome:'Fulano de Tal',plano:"BASIC",mes:{barba:3,cabelo:2,depilação:1},total:{cabelos:22,barbas:32,depilacao:5},barbearia:"BARBEARIA BAIRRO BONFIM"}
-]
-const listaPlanos = [
-    {nome:"BASIC",preco:"R$30,00",barbas:1,cabelos:1,img:"images/barbeiros/barbeiro_detalhe_barba_tesoura.jpg",vantagens:["Barbas mais robustas","Ajustes mensais"],economia:20},
-    {nome:"STANDARD",preco:"R$65,00",barbas:2,cabelos:3,img:"images/barbeiros/barbeiro_detalhe_barba.jpg",vantagens:["Atendentes","Escolha de Barbeiro","Horários fixos"],economia:25},
-    {nome:"PREMIUM",preco:"R$80,00",barbas:5,cabelos:5,img:"images/barbeiros/barbeiro_detalhe_tesoura.jpg",vantagens:["Preferência nos agendamentos","Maior flexibilidade de agendamento","Atendimento Luxo"],economia:30},
-]
+const listaUsuarios = require('../database/usuarios');
+
+const listaPlanos = require('../database/planos')
 
 
 const userController = {
---
+++ database/usuarios.js
@@ -0,0 +1,9 @@
+const listaUsuarios =[
+    {nome:'Fulano de Tal',
+    plano:"BASIC",
+    mes:{barba:3,cabelo:2,depilação:1},
+    total:{cabelos:22,barbas:32,depilacao:5},
+    barbearia:"BARBEARIA BAIRRO BONFIM"
+},]
+
+module.exports = listaUsuarios;
