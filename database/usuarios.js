2e6e286aaa96a7c894d1be1617e3e9a1e5b15f3c
Julio Mattos <juliocastellobranco@gmail.com>
2022-07-14 00:50:37 -0300

    usuarios in databasefolder

---
+++ database/usuarios.js
@@ -0,0 +1,10 @@
+const usuarios = [
+    {nome:'Fulano de Tal',
+    plano:"BASIC",
+    mes:{barba:3,cabelo:2,depilação:1},
+    total:{cabelos:22,barbas:32,depilacao:5},
+    barbearia:"BARBEARIA BAIRRO BONFIM"
+}
+]
+
+module.exports = usuarios;
