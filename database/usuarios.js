3897c46a9ce2d06b446ce1486c7d010269891bb3
melissa ost <melissa-ost@hotmail.com>
2022-07-15 12:28:24 -0300

    adicionando tags ejs carrinho

--- database/usuarios.js
+++ database/usuarios.js
@@ -4,6 +4,13 @@ const listaUsuarios =[
     mes:{barba:3,cabelo:2,depilação:1},
     total:{cabelos:22,barbas:32,depilacao:5},
     barbearia:"BARBEARIA BAIRRO BONFIM"
-},]
+},{nome:'Fulano de Tal',
+plano:"PREMIUM",
+mes:{barba:3,cabelo:2,depilação:1},
+total:{cabelos:22,barbas:32,depilacao:5},
+barbearia:"BARBEARIA BAIRRO BONFIM"
+}]
+
+
 
 module.exports = listaUsuarios;
