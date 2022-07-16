05b619930664bd6ebb8b5866f960cf54e2bbf652
MelissaOst <melissa-ost@hotmail.com>
2022-07-16 15:11:01 -0300

    transformando o preco em inteiro

--- database/planos.js
+++ database/planos.js
@@ -1,6 +1,6 @@
 const listaPlanos = [
     {nome:"BASIC",
-    preco:"R$30,00",
+    preco:30,
     barbas:1,
     cabelos:1,
     img:"images/barbeiros/barbeiro_detalhe_barba_tesoura.jpg",
@@ -8,7 +8,7 @@ const listaPlanos = [
     economia:20}
     ,
     {nome:"STANDARD",
-    preco:"R$65,00",
+    preco:65,
     barbas:2,
     cabelos:3,
     img:"images/barbeiros/barbeiro_detalhe_barba.jpg",
@@ -16,7 +16,7 @@ const listaPlanos = [
     economia:25}
     ,
     {nome:"PREMIUM",
-    preco:"R$80,00",
+    preco:80,
     barbas:5,
     cabelos:5,
     img:"images/barbeiros/barbeiro_detalhe_tesoura.jpg",
@@ -24,7 +24,7 @@ const listaPlanos = [
     economia:30}
     ,
     {nome:"MASTER",
-    preco:"R$100,00",
+    preco:100,
     barbas: 'Ilimitado',
     cabelos:'Ilimitado',
     img:"images/barbeiros/barbeiro_detalhe_tesoura.jpg",
