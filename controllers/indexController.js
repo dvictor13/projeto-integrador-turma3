a4d9cf5e4726ed5d558a1164e443b7796845f08e
Douglas Victor Menezes <dv@douglasvictor.com>
2022-09-07 21:35:04 -0300

    inseri pags equipe politica e faleconosco

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -7,6 +7,16 @@ const indexController = {
     home:(req,res)=>{
         res.render('index',{listaplanos:listaPlanos,barbearias:listaBarbearias});
     },
+    equipe:(req,res)=>{
+        res.render('equipe');
+    },
+    politica:(req,res)=>{
+        res.render('politica');
+    },
+    faleconosco:(req,res)=>{
+        res.render('faleconosco');
+    },
+
 }
 
 module.exports = indexController;
