0e8b5ba4a30c77a464b4fcf0b0c8b9bc352394aa
MelissaOst <melissa-ost@hotmail.com>
2022-09-25 20:19:04 -0300

    filtro por servico

--- public/js/barbearias.js
+++ public/js/barbearias.js
@@ -1,4 +1,3 @@
-
 const input = document.getElementById('nome');
 const ul = document.querySelector(".cards-container");
 
@@ -15,5 +14,35 @@ input.addEventListener('input', event => {
         todo.style.display = 'flex'
     })
 
+})
+
+
+const inputSelect = document.getElementById('departamentos');
+const btnform = document.querySelector('.btnform')
+const limpar = document.querySelector('.limpar')
+
+btnform.addEventListener('click', event => {
+    console.log(inputSelect.value)
+    Array.from(ul.children)
+    .filter(todo => !todo.textContent.toLowerCase().includes(inputSelect.value))
+    .forEach(todo => {
+        todo.style.display = 'none'
+    })
+    Array.from(ul.children)
+    .filter(todo => todo.textContent.toLowerCase().includes(inputSelect.value))
+    .forEach(todo => {
+        todo.style.display = 'flex'
+    })
+
+})
+
+limpar.addEventListener('click', event =>{
+    Array.from(ul.children)
+    .forEach(todo => {
+        todo.style.display = 'flex'
+    })
+
+    inputSelect.selectedIndex = 0
+    input.value = ''
 })
    
