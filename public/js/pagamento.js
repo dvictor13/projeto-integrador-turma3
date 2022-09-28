b100a440ab5ed3bdb627b2378b83dec442220f17
Julio Mattos <juliocastellobranco@gmail.com>
2022-09-28 16:46:53 -0300

    add api ibge

--- public/js/pagamento.js
+++ public/js/pagamento.js
@@ -10,24 +10,27 @@ escolhaTempoPlano.addEventListener('change',function(){
 
     total.innerHTML = parseFloat(planopreco.innerHTML) * parseFloat(escolhaTempoPlano.value) 
 })
+
 const getEstados = async ()=>{
-    let res = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome',{method:'get'})
-    let distritos = await res.json();
-    return distritos
-
+    let response = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome',{method:'get'})
+    let distritos = await response.json();
+    s = document.getElementById('estado');
+    distritos.forEach(distrito =>{ s.appendChild(new Option(distrito.nome,distrito.sigla))})
 }
 
 
-const showEstados = ()=>{
-    const s = document.getElementById('estado');
-    const options = distritos;
-    console.log(options)
+const getPaises = async ()=>{
+    let respaises = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/paises?orderBy=nome',{method:'get'})
+    let paises = await respaises.json();
+    p = document.getElementById('pais');
+    paises.forEach(pais =>{ p.appendChild(new Option(pais.nome,pais.nome))})
+    // console.log(paises);
+    return paises
+}
 
-    for (const option in options){
-        s.appendChild(new Option(option.name,option.sigla));    
-}
-}
-window.onload = (event) =>{
-    distritos = getEstados()
-    showEstados()
-}
+
+getEstados()
+//showEstados(distritos)
+getPaises()
+
+
